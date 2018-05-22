fe942990b54629c95a732e62c501a4b5c6fd949e
Lauren Richert <lsrichert@gmail.com>
2018-05-22 15:22:07 -0500

    added JS

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-document.querySelector()
+const articleRef = document.querySelector("#congressional-rep")
+console.log(articleRef)
+
+let paragraphElement = document.createElement("p")
+let paragraphTextNode = document.createTextNode("Made up mission statement")
+paragraphElement.appendChild(paragraphTextNode)
+console.log(paragraphElement);
+
+articleRef.appendChild(paragraphElement)
+
+const congressionalDistAttr = document.createAttribute("congressional-district")
+congressionalDistAttr.value = "5"
+
+articleRef.setAttributeNode(congressionalDistAttr)
